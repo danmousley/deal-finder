ccd2f317868cf3e5e1ef63a6f3ef01b7ce85680b
danmousley <dannymousley@outlook.com>
2021-12-20 15:26:13 +0700

    added user and item models

--- amazon/models.py
+++ amazon/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+class Item(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name = "followed_items")
+    name = models.CharField(max_length=64)
+    title = name = models.CharField(max_length=64)
+    original_price = models.DecimalField(max_digits=12, decimal_places=2)
+    current_price = models.DecimalField(max_digits=12, decimal_places=2)
+    is_deal = models.BooleanField(default=False)
+
